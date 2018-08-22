a9fd91309d0ec9b48c87c7c207c504df660253b9
opet <opet@Academico.edu>
2018-08-22 09:13:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/opet/meuprimeiroappopet/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.opet.meuprimeiroappopet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+    }
+
+    public void clique(View view){
+        String texto = editText.getText().toString();
+        if(view.getId() == R.id.btnClique){
+            Toast.makeText(this, texto, Toast.LENGTH_LONG).show();
+        }
+    }
+}
